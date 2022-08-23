21b7b67b3f397739424908e8fe5b647f534b160d
tossapon <tossapon.nga@mahidol.ac.th>
2022-08-23 16:10:50 +0700

    Merge from dev and Run build

---
+++ public/build/assets/DivisionService.2e8f00a7.js
@@ -0,0 +1,1 @@
+import{a as i}from"./app.351f6ed1.js";class r{listAll(){return i.get(route("admin.list_division_all")).then(t=>t.data)}listBranchOnly(){return i.get(route("admin.list_branch_only")).then(t=>t.data)}}export{r as D};
