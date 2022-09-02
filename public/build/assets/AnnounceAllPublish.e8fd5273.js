cfbe190c88a77943a0feeb9113ea424e9e7a14f7
mse4694 <tossaponn@hotmail.com>
2022-09-02 21:41:45 +0700

    Merge from dev and Run build

---
+++ public/build/assets/AnnounceAllPublish.e8fd5273.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Announcement.b66cf29e.js";import{o as r,c as t}from"./app.5ef6262d.js";import"./th.4cb7964f.js";import"./buddhistEra.c5862533.js";const i={__name:"AnnounceAllPublish",setup(c){return(e,_)=>(r(),t(o,{limit:0}))}};export{i as default};
