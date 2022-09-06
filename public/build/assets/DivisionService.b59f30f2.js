f4875042e07bb052971191ae0a68e6fef3ec1b18
mse4694 <tossaponn@hotmail.com>
2022-09-06 11:56:04 +0700

    Merge from dev and Run build

---
+++ public/build/assets/DivisionService.b59f30f2.js
@@ -0,0 +1,1 @@
+import{a as i}from"./app.b7881b70.js";class r{listAll(){return i.get(route("admin.list_division_all")).then(t=>t.data)}listBranchOnly(){return i.get(route("admin.list_branch_only")).then(t=>t.data)}}export{r as D};
