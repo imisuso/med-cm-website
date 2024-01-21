4af1a93377733dd9994f35a07cae37e58cc3705a
mse4694 <tossaponn@hotmail.com>
2024-01-21 21:42:44 +0700

    Merge form main and Run build

---
+++ public/build/assets/DivisionService.52549adb.js
@@ -0,0 +1,1 @@
+import{E as i}from"./app.5f1fd476.js";class r{listAll(){return i.get(route("admin.list_division_all")).then(t=>t.data)}listBranchOnly(){return i.get(route("admin.list_branch_only")).then(t=>t.data)}}export{r as D};
