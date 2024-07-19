958a97c48a553c3f69297ccf9cfc449ea9b3a8ac
tossapon <tossapon.nga@mahidol.ac.th>
2024-07-19 09:18:36 +0700

    fix burger menu when mobile screen and edit component Link

--- resources/js/Layouts/Nav.vue
+++ resources/js/Layouts/Nav.vue
@@ -118,11 +118,9 @@
         </div>
 
         <!-- Mobile Side nav -->
-          <!-- <div id="mobile-nav" class="visible lg:hidden flex items-center overflow-scroll"> -->
           <div id="mobile-nav" class="visible lg:hidden flex items-center z-50 bg-gray-800 bg-opacity-25">
             <div v-show="isMenuOpen" id="mobile-overlay" class=" flex overflow-x-hidden overflow-y-auto fixed top-0 left-0 right-0 inset-0 z-50 justify-center sm:h-full bg-gray-800 bg-opacity-25">
               <ul v-show="isMenuOpen" class="z-40 w-10/12 md:w-2/5 max-h-screen p-4 border-r bg-green-800 absolute rounded -top-16 left-0 right-0 shadow mt-16 md:mt-16 hidden overflow-scroll">
-                <!-- <div class="visible lg:hidden flex overflow-x-hidden overflow-y-auto fixed top-0 left-0 right-0 md:inset-0 z-50 justify-center sm:h-full bg-gray-800 bg-opacity-25"> -->
                 <li>
                   <div class="flex items-center w-full justify-between">
                     <div class="flex">
@@ -152,7 +150,6 @@
                               ศิริราช
                               <br />
                               <span class="relative px-0">
-                                <!-- <div class="absolute inset-x-0 inset-y-1 bottom-0 h-5 transform -skew-x-12 bg-yellow-400"></div> -->
                                 <span class="relative inline-block text-white text-lg italic">อายุรศาสตร์</span>
                               </span>
                           </h2>
@@ -160,13 +157,13 @@
                       </Link>
                     </div>
                     <div>
-                      <div class="hidden close-m-menu cursor-pointer" @click="SideMenuHandler($event, false)">
-                          <svg aria-label="Close" v-bind:xmlns="xmlns" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                              <path stroke="none" d="M0 0h24v24H0z" />
-                              <line x1="18" y1="6" x2="6" y2="18" />
-                              <line x1="6" y1="6" x2="18" y2="18" />
-                          </svg>
-                      </div>
+                        <div class="hidden close-m-menu cursor-pointer" @click="SideMenuHandler($event, false)">
+                            <svg aria-label="Close" v-bind:xmlns="xmlns" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <line x1="18" y1="6" x2="6" y2="18" />
+                                <line x1="6" y1="6" x2="18" y2="18" />
+                            </svg>
+                        </div>
                     </div>
                   </div>
                 </li>
@@ -227,7 +224,7 @@
                 <line x1="4" y1="16" x2="20" y2="16"></line>
             </svg> -->
 
-            <svg @click="MenuHandler($event, true)" aria-haspopup="true" aria-label="Main Menu" v-bind:xmlns="xmlns" class="w-7 text-emerald-400 show-m-menu icon icon-tabler icon-tabler-menu dropsidemenu cursor-pointer" viewBox="0 0 24 24">
+            <svg @click="setIsOpen(true)" aria-haspopup="true" aria-label="Main Menu" v-bind:xmlns="xmlns" class="w-7 text-emerald-400 show-m-menu icon icon-tabler icon-tabler-menu dropsidemenu cursor-pointer" viewBox="0 0 24 24">
               <path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
               <path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
               <path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
@@ -246,11 +243,146 @@
     </nav>
   </div>
 
+    <TransitionRoot appear as="template" :show="isOpen">
+        <Dialog
+            :initial-focus="closeMenuButtonRef"
+            class="fixed inset-0 overflow-hidden z-10"
+            :open="isOpen" @close="setIsOpen">
+            <TransitionChild
+                enter="transition-opacity ease-in-out duration-300"
+                enter-from="-translate-x-full"
+                enter-to="translate-x-0"
+                leave="transition-opacity ease-in-out duration-300"
+                leave-from="translate-x-0"
+                leave-to="-translate-x-full"
+                as="template">
+                <DialogOverlay class="absolute inset-0 bg-black bg-opacity-40"></DialogOverlay>
+            </TransitionChild>
+
+            <TransitionChild
+                enter="transition-opacity ease-linear duration-200"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="transition-opacity ease-linear duration-200"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+                as="template">
+                <div class="flex flex-col fixed inset-y-0 left-0 w-10/12 md:w-2/5 max-w-sm bg-green-800">
+                    <div class="flex items-center justify-between p-4 shadow ">
+                        <DialogTitle class="text-lg font-semibold">
+                            <div class="flex">
+                                <Link
+                                    :href="route('index')"
+                                    aria-label="SI MEDICINE"
+                                    title="SI MEDICINE"
+                                    class="inline-flex items-center"
+                                >
+                                    <svg
+                                        class="w-8 text-emerald-400"
+                                        viewBox="0 0 24 24"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        stroke-linecap="round"
+                                        stroke-miterlimit="10"
+                                        stroke="currentColor"
+                                        fill="none"
+                                    >
+                                        <rect x="3" y="1" width="7" height="12"></rect>
+                                        <rect x="3" y="17" width="7" height="6"></rect>
+                                        <rect x="14" y="1" width="7" height="6"></rect>
+                                        <rect x="14" y="11" width="7" height="12"></rect>
+                                    </svg>
+                                <span class="ml-2 tracking-wide text-white uppercase">
+                                  ศิริราช
+                                  <br />
+                                  <span class="relative px-0">
+                                    <span class="relative  text-white text-lg italic">อายุรศาสตร์</span>
+                                  </span>
+                                </span>
+                                </Link>
+                            </div>
+                        </DialogTitle>
+                        <button @click="setIsOpen(false)"
+                                class="p-1 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                                ref="closeMenuButtonRef"
+                        >
+                            <XIcon class="w-6 h-6"></XIcon>
+                        </button>
+                    </div>
+
+                    <div class="flex flex-col flex-1 px-4 overflow-y-auto">
+                        <ul>
+                            <li class="mb-4">
+                                <hr class="border-b border-white w-full" />
+                            </li>
+                            <template v-for="main_menu_item in main_menu" :key="main_menu_item.main_menu_id">
+                                <li v-if="($page.url === '/' && main_menu_item.main_menu_id === 1) || ($page.url.startsWith('/#') && main_menu_item.main_menu_id === 1)" class="dropsidemenu">
+                                    <MobileMainMenu
+                                        :has_sub_menu="main_menu_item.has_sub_menu"
+                                        :pic_heading_submenu="pic_heading_submenu"
+                                        :menu_name="main_menu_item.main_menu_name"
+                                        :menu_link="main_menu_item.main_menu_link"
+                                        :sub_menu="filterSubMenuByID(sub_menu, main_menu_item.main_menu_id)"
+                                        @click-tag="setIsOpen(false)"
+                                    />
+                                </li>
+                                <li v-else-if="main_menu_item.main_menu_id === 1">
+                                    <Link :href="route('index')" class="font-medium cursor-pointer tracking-wide text-menu-color transition-colors duration-200">
+                                        <button class="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                                            </svg>
+                                        </button>
+                                    </Link>
+                                </li>
+                                <li v-else>
+                                    <MobileMainMenu
+                                        :has_sub_menu="main_menu_item.has_sub_menu"
+                                        :pic_heading_submenu="pic_heading_submenu"
+                                        :menu_name="main_menu_item.main_menu_name"
+                                        :menu_link="main_menu_item.main_menu_link"
+                                        :sub_menu="filterSubMenuByID(sub_menu, main_menu_item.main_menu_id)"
+                                    />
+                                </li>
+                            </template>
+                            <li class="mb-4">
+                                <hr class="border-b border-white w-full" />
+                            </li>
+                            <Link
+                                :href="route('login')"
+                                class="inline-flex items-center justify-center h-10 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-emerald-500 hover:bg-emerald-600 focus:shadow-outline focus:outline-none"
+                                aria-label="Sign in"
+                                title="Sign in"
+                                @click="setIsOpen(false)"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                                </svg>
+                            </Link>
+                        </ul>
+                    </div>
+
+
+                </div>
+            </TransitionChild>
+        </Dialog>
+    </TransitionRoot>
+
 </template>
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
 import { Link } from '@inertiajs/vue3'
+import {
+    Dialog,
+    DialogOverlay,
+    DialogPanel,
+    DialogTitle,
+    DialogDescription,
+    TransitionRoot,
+    TransitionChild
+} from '@headlessui/vue'
+import {XIcon,} from '@heroicons/vue/solid';
 import MobileMainMenu from '@/Components/MobileMainMenu.vue'
 
 onMounted(() => {
@@ -270,10 +402,17 @@ const pic_heading_submenu = ref('<svg xmlns="http://www.w3.org/2000/svg" class="
 const isMenuOpen = ref(false)
 const isShow = ref(false)
 const isScroll = ref(false)
+let closeMenuButtonRef =ref(null)
 //const items_visibility = ref([true, true, true, true]);
 
 const toggle_menu = (event) => {
   isMenuOpen.value = false
+}
+
+const isOpen = ref(false)
+
+function setIsOpen(value) {
+    isOpen.value = value
 }
 
 // const toggle_visibleby_class = (index) => {
@@ -339,6 +478,8 @@ const updateScroll = (event) => {
 
 const MenuHandler = (el, val) => {
 
+    console.log('Click')
+    // console.log(val)
   let MainList = el.currentTarget.parentElement.getElementsByTagName("ul")[0];
   let closeIcon = el.currentTarget.parentElement.getElementsByClassName("close-m-menu")[0];
   //let showIcon = el.currentTarget.parentElement.getElementsByClassName("show-m-menu")[0];
@@ -353,7 +494,9 @@ const MenuHandler = (el, val) => {
       // el.currentTarget.classList.add("hidden");
   }
 
+  console.log('Before Click = ' + isMenuOpen.value)
   isMenuOpen.value = val
+  console.log('After Click = ' + isMenuOpen.value)
 }
 
 const SideMenuHandler = (el, val) => {
