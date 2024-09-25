c59b1bdcb4ae4f02aaa53750af6228539a9c1b2a
tossapon <tossapon.nga@mahidol.ac.th>
2024-09-25 15:42:56 +0700

    Upgate version and Test vue-pdf-embeded, swiper

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import react from '@vitejs/plugin-react';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/js/app.js',
-        ]),
-        // react(),
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
         vue({
             template: {
                 transformAssetUrls: {
