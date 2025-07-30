648cd8ef01d03b438585ebc274366422c10b69c7
tossapon <tossapon.nga@mahidol.ac.th>
2025-07-30 15:41:24 +0700

    Merge from upgrade_laravel_11to12 and Run build

--- public/build/assets/DivisionService-CYdiY4jK.js
+++
@@ -1,1 +0,0 @@
-import{R as i}from"./app-ZFNoSTGk.js";class r{listAll(){return i.get(route("admin.list_division_all")).then(t=>t.data)}listBranchOnly(){return i.get(route("admin.list_branch_only")).then(t=>t.data)}}export{r as D};
