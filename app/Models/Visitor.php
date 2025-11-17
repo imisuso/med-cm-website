473094dcd405e4124c9bcf4504ab72b3bea320cf
tossapon <tossapon.nga@mahidol.ac.th>
2025-11-17 15:08:54 +0700

    Data Sanitization in Visitor Model

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -21,4 +21,16 @@ class Visitor extends Model
 //        'date' => 'datetime:Y-m-d',
 //    ];
 
+// เพิ่มฟังก์ชันนี้เข้าไปใน Model ที่มีปัญหา เพื่อดักจับตัวอักษรเสียๆ เวลาดึงข้อมูลมาแสดง
+    public function toArray()
+    {
+        $attributes = parent::toArray();
+        array_walk_recursive($attributes, function (&$value) {
+            if (is_string($value) && !mb_check_encoding($value, 'UTF-8')) {
+                $value = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
+            }
+        });
+        return $attributes;
+    }
+
 }
