6676182076d55991d78e30e7ff8a378f2d421fe0
tossapon <tossapon.nga@mahidol.ac.th>
2025-11-24 11:32:11 +0700

    Merge from dev-quill and Run build

--- public/build/assets/DivisionService-DO70qBe3.js
+++
@@ -1,1 +0,0 @@
-import{x as i}from"./app-D_DQPl2C.js";class r{listAll(){return i.get(route("admin.list_division_all")).then(t=>t.data)}listBranchOnly(){return i.get(route("admin.list_branch_only")).then(t=>t.data)}}export{r as D};
